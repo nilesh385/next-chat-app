a14f867ebc414d556f878cf10dbc04d0a4d507bc
Nilesh Kumar <nileshandnilesh120@gmail.com>
2024-05-15 10:45:23 +0530

    creating group and deleting group added

--- convex/conversation.ts
+++ convex/conversation.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { getUserByClerkId } from "./_utils";
 
 export const get = query({
@@ -59,6 +59,115 @@ export const get = query({
         },
         otherMembers: null,
       };
+    } else {
+      const otherMembers = await Promise.all(
+        allConversationMemberships
+          .filter((membership) => membership.memberId !== currentUser._id)
+          .map(async (membership) => {
+            const member = await ctx.db.get(membership.memberId);
+            if (!member) {
+              throw new ConvexError("Member could not be found");
+            }
+            return { username: member.username };
+          })
+      );
+      return { ...conversation, otherMembers, otherMember: null };
     }
+  },
+});
+
+export const createGroup = mutation({
+  args: {
+    members: v.array(v.id("users")),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const currentUser = await getUserByClerkId({
+      ctx,
+      clerkId: identity.subject,
+    });
+
+    if (!currentUser) {
+      throw new ConvexError("User not found");
+    }
+
+    const conversationId = await ctx.db.insert("conversations", {
+      isGroup: true,
+      name: args.name,
+    });
+
+    await Promise.all(
+      [...args.members, currentUser._id].map(async (memberId) => {
+        await ctx.db.insert("conversationMembers", {
+          memberId,
+          conversationId,
+        });
+      })
+    );
+  },
+});
+
+export const deleteGroup = mutation({
+  args: {
+    conversationId: v.id("conversations"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Unauthorized");
+    }
+
+    const currentUser = await getUserByClerkId({
+      ctx,
+      clerkId: identity.subject,
+    });
+
+    if (!currentUser) {
+      throw new ConvexError("User not found");
+    }
+
+    const conversation = await ctx.db.get(args.conversationId);
+
+    if (!conversation) {
+      throw new ConvexError("Conversation not found.");
+    }
+
+    const memberships = await ctx.db
+      .query("conversationMembers")
+      .withIndex("by_conversationId", (q) =>
+        q.eq("conversationId", args.conversationId)
+      )
+      .collect();
+
+    if (!memberships || memberships.length <= 1) {
+      throw new ConvexError("This conversation does not have any member.");
+    }
+
+    const messages = await ctx.db
+      .query("messages")
+      .withIndex("by_conversationId", (q) =>
+        q.eq("conversationId", args.conversationId)
+      )
+      .collect();
+
+    await ctx.db.delete(args.conversationId);
+
+    await Promise.all(
+      memberships.map(async (membership) => {
+        await ctx.db.delete(membership._id);
+      })
+    );
+    await Promise.all(
+      messages.map(async (message) => {
+        await ctx.db.delete(message._id);
+      })
+    );
   },
 });
